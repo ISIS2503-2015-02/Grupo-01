23603ce10968e3bcadff7e0d2a41338aac74e578
Santiago <s.beltran10@uniandes.edu.co>
2015-08-18 23:59:48 -0500

    clases controllers creadas para servicios faltantes

---
+++ app/controllers/EstacionController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Estacion;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
+
+public class Estacion  extends Controller{
+	
+}
